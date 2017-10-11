66cc7b7320ae881a55901ca3ce2cb6939af9c4ce
MelaniePetit <melanie.petitcuenot@etu.univ-lyon1.fr>
2017-10-11 11:24:27 +0200

    homepage fragment

--- app/src/main/java/com/example/leamelanie/polytechandroid1/MainActivity.java
+++ app/src/main/java/com/example/leamelanie/polytechandroid1/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.buttonPDG);
+        Button button = (Button) findViewById(R.id.OnevsIA);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
