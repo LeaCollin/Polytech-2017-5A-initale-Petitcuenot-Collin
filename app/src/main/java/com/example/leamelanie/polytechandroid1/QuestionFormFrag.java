f0c9190ac8450123cf1452d78f363009e6374690
MelaniePetit <melanie.petitcuenot@etu.univ-lyon1.fr>
2017-10-20 09:25:48 +0200

    Answer service

--- app/src/main/java/com/example/leamelanie/polytechandroid1/QuestionFormFrag.java
+++ app/src/main/java/com/example/leamelanie/polytechandroid1/QuestionFormFrag.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.annotation.BoolRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.leamelanie.polytechandroid1.service.MyIntentService;
@@ -24,21 +26,39 @@ public class QuestionFormFrag extends Fragment {
 
         public static final String ACTION_RESP = "actionResp";
 
+
         @Override
         public void onReceive(Context context, Intent intent) {
             String myQuestion = intent.getStringExtra(MyIntentService.QUESTION);
-            TextView textView = getActivity().findViewById(R.id.question);
-            textView.setText(myQuestion);
+            TextView textViewQuestion = getActivity().findViewById(R.id.question);
+            textViewQuestion.setText(myQuestion);
+
+            Boolean myAnswer = intent.getBooleanExtra(MyIntentService.ANSWER, false);
+            TextView textViewScore = getActivity().findViewById(R.id.score);
+            if (myAnswer){
+                textViewScore.setText("1");
+            }
+            else {
+                textViewScore.setText("-1");
+
+            }
+
+
         }
     }
 
     private MyReceiver receiver;
+    public int score = 0;
+    public Button butAnswer1 = null;
+    public Button butAnswer2 = null;
+    public Button butAnswer3 = null;
+    public Button butAnswer4 = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_question_form, container, false);
 
+        return inflater.inflate(R.layout.frag_question_form, container, false);
 
     }
 
@@ -47,11 +67,51 @@ public class QuestionFormFrag extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        TextView textViewMyScore = getActivity().findViewById(R.id.score);
+        textViewMyScore.setText(String.valueOf(score));
+        butAnswer1 = getActivity().findViewById(R.id.button1);
+        butAnswer2 = getActivity().findViewById(R.id.button2);
+        butAnswer3 = getActivity().findViewById(R.id.button3);
+        butAnswer4 = getActivity().findViewById(R.id.button4);
+
         receiver = new MyReceiver();
 
         Intent myIntent = new Intent(getActivity(), MyIntentService.class);
         myIntent.putExtra(MyIntentService.TASK, "questionA");
         getActivity().startService(myIntent);
+
+        Button.OnClickListener answerListener = new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getActivity(), MyIntentService.class);
+
+                switch (v.getId()){
+                    case R.id.button1:
+                        myIntent.putExtra(MyIntentService.TASK, butAnswer1.getText());
+                        getActivity().startService(myIntent);
+                        break;
+                    case R.id.button2:
+                        myIntent.putExtra(MyIntentService.TASK, butAnswer2.getText());
+                        getActivity().startService(myIntent);
+                        break;
+                    case R.id.button3:
+                        myIntent.putExtra(MyIntentService.TASK, butAnswer3.getText());
+                        getActivity().startService(myIntent);
+                        break;
+                    case R.id.button4:
+                        myIntent.putExtra(MyIntentService.TASK, butAnswer4.getText());
+                        getActivity().startService(myIntent);
+                        break;
+
+                }
+            }
+
+        };
+
+        butAnswer1.setOnClickListener(answerListener);
+        butAnswer2.setOnClickListener(answerListener);
+        butAnswer3.setOnClickListener(answerListener);
+        butAnswer4.setOnClickListener(answerListener);
     }
 
     @Override
@@ -61,5 +121,9 @@ public class QuestionFormFrag extends Fragment {
         IntentFilter filter = new IntentFilter(MyReceiver.ACTION_RESP);
         filter.addCategory(Intent.CATEGORY_DEFAULT);
         getActivity().registerReceiver(receiver, filter);
+    }
+
+    public void setOnClick(final Button btn){
+
     }
 }
--- app/src/main/java/com/example/leamelanie/polytechandroid1/service/MyIntentService.java
+++ app/src/main/java/com/example/leamelanie/polytechandroid1/service/MyIntentService.java
@@ -2,11 +2,9 @@ package com.example.leamelanie.polytechandroid1.service;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.support.annotation.Nullable;
 
 import com.example.leamelanie.polytechandroid1.QuestionFormFrag;
 
-import java.util.ArrayList;
 
 /**
  * Created by Mel on 18/10/2017.
@@ -16,6 +14,7 @@ public class MyIntentService extends IntentService {
 
     public static final String TASK = "task";
     public static final String QUESTION = "question";
+    public static final String ANSWER = "answer";
 
     public String nextQuestion = "De quel animal est-ce le cri ?";
 
@@ -27,11 +26,27 @@ public class MyIntentService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         String myTask = intent.getStringExtra(TASK);
 
+        if (myTask == "questionA") {
             Intent broadcastIntent = new Intent();
             broadcastIntent.setAction(QuestionFormFrag.MyReceiver.ACTION_RESP);
             broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
             broadcastIntent.putExtra(QUESTION, nextQuestion);
             sendBroadcast(broadcastIntent);
+        }
+        else if(myTask == "Le Chien"){
+            Intent broadcastIntent = new Intent();
+            broadcastIntent.setAction(QuestionFormFrag.MyReceiver.ACTION_RESP);
+            broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+            broadcastIntent.putExtra(ANSWER, true);
+            sendBroadcast(broadcastIntent);
+        }
+        else {
+            Intent broadcastIntent = new Intent();
+            broadcastIntent.setAction(QuestionFormFrag.MyReceiver.ACTION_RESP);
+            broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+            broadcastIntent.putExtra(ANSWER, false);
+            sendBroadcast(broadcastIntent);
+        }
 
     }
 }
